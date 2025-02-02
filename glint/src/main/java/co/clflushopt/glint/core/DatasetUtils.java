3e7e43271b308d7496c6879f7c2e1b09442870db
clflushopt <172141496+clflushopt@users.noreply.github.com>
2025-02-02 15:22:54 -0500

    fix: Github action messing up -- add accidently gitignored files

---
+++ glint/src/main/java/co/clflushopt/glint/core/DatasetUtils.java
@@ -0,0 +1,31 @@
+package co.clflushopt.glint.core.datasets;
+
+import java.util.Arrays;
+
+import co.clflushopt.glint.types.ArrowTypes;
+import co.clflushopt.glint.types.Field;
+import co.clflushopt.glint.types.Schema;
+
+public class DatasetUtils {
+
+    public static final Schema getNYCYellowTripsCSVSchema() {
+        return new Schema(Arrays.asList(new Field("VendorID", ArrowTypes.Int32Type),
+                new Field("tpep_pickup_datetime", ArrowTypes.StringType),
+                new Field("tpep_dropoff_datetime", ArrowTypes.StringType),
+                new Field("passenger_count", ArrowTypes.Int32Type),
+                new Field("trip_distance", ArrowTypes.DoubleType),
+                new Field("RatecodeID", ArrowTypes.Int32Type),
+                new Field("store_and_fwd_flag", ArrowTypes.StringType),
+                new Field("PULocationID", ArrowTypes.Int32Type),
+                new Field("DOLocationID", ArrowTypes.Int32Type),
+                new Field("payment_type", ArrowTypes.Int32Type),
+                new Field("fare_amount", ArrowTypes.DoubleType),
+                new Field("extra", ArrowTypes.DoubleType),
+                new Field("mta_tax", ArrowTypes.DoubleType),
+                new Field("tip_amount", ArrowTypes.DoubleType),
+                new Field("tolls_amount", ArrowTypes.DoubleType),
+                new Field("improvement_surcharge", ArrowTypes.DoubleType),
+                new Field("total_amount", ArrowTypes.DoubleType)));
+    }
+
+}
